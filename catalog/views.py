00829cab00b5bb6a35444ffb9f9257ac8814bbb5
maria <buzinamn@yandex.ru>
2023-09-12 14:45:21 +0500

    homework_20.2

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from catalog.models import Product
+
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list,
+        'title': 'Сытый барин - Главная'
+    }
+    return render(request, 'catalog/home.html', context)
 
 def contacts(request):
-    return render(request, 'catalog/contacts.html')
+    context = {
+        'title': 'Сытый барин - Контакты'
+    }
+    return render(request, 'catalog/contacts.html', context)
+
+
+def product(request, pk):
+    products_list = Product.objects.get(pk=pk)
+    context = {
+        'object_list': products_list,
+        'title': 'Сытый барин - Информация о продукте'
+    }
+    return render(request, 'catalog/product.html', context)
